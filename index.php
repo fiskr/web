2db270086a8e31501403608cd51f693e8c22b2ef
brando26 <brando26@moya.asoshared.com>
2015-11-24 12:16:25 -0500

    adding favicon to the homepage

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-
+  <head>
+    <link href="./css/markdown.css"/>
+    <link href="img/apple-favicon.png" rel="shortcut icon" />
+  </head>
   <body>
     <h1> HELLO WORLD </h1>
     <?php
