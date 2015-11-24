8a84cc477c28470aabe363da595506f947928730
Brandon Foster <bfoster@scrippsnetworks.com>
2015-11-24 12:42:07 -0500

    adding Google Ad

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
     <?php
       echo 'whoa'
     ?>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- SPEAK -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:300px;height:250px"
+         data-ad-client="ca-pub-7685740874128456"
+         data-ad-slot="4872128524"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
   </body>
 
 </html>
