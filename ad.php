035a0a1bd33c9d9ea1a310c8ab651320171fe3bd
brando26 <brando26@moya.asoshared.com>
2015-12-07 15:30:07 -0500

    updating ads

--- ad.php
+++ ad.php
@@ -1,9 +1,9 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Google Ad -->
+<!-- Ironic Google Ad -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:300px;height:250px"
      data-ad-client="ca-pub-7685740874128456"
-     data-ad-slot="4872128524"></ins>
+     data-ad-slot="4776226923"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
